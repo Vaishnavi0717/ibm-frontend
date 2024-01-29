a4176a8cb6876ebe1c5efdf278d6e2ac97002667
Vaishnavi0717 <vaishnavipunekar0@.com>
2024-01-29 12:50:51 +0530

    finalupdates

--- src/Components/Bug.jsx
+++ src/Components/Bug.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Bug = () => {
   return (
     <div>
-      <img className='w-full m-auto h-screen' src="https://b5digital.dk/wp-content/uploads/2022/06/bug-tracking-tools.jpg" alt ="bugTracker" />
+      <img className='w-full m-auto h-screen' src="https://uploads-ssl.webflow.com/615af81f65d1ab72d2969269/6176011ed02509f4285f2595_bug-reports-750x365.jpg" alt ="bugTracker" />
       
     </div>
   )
